7f553a1890fb726fcdb3a8e08daedd52fe712917
Artur Witkowski <a.witkowski99@gmail.com>
2020-06-06 02:24:00 +0200

    Renaming variable

--- backend/app.js
+++ backend/app.js
@@ -28,10 +28,10 @@ app.use((error, req, res, next) => {
 const server = app.listen(8080);
 const io = require('./socket').init(server);
 io.on('connect', socket => {
-    let licz = 0;
+    let counter = 0;
     console.log('Client connected');
     
     socket.on('lineTo', data => {
-        console.log(`${licz++}. ${data.x}, ${data.y} - ${data.color}`);
+        console.log(`${counter++}. ${data.x}, ${data.y} - ${data.color}`);
     })
 })
