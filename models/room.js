c9975f9e5f450c5e6fabf64cdf607d231e106b80
Artur Witkowski <a.witkowski99@gmail.com>
2020-06-17 00:12:45 +0200

    Develop Room schema

--- models/room.js
+++ models/room.js
@@ -6,7 +6,40 @@ const roomSchema = new Schema({
   roomId: {
     type: String,
     required: true
-  }
+  },
+  gameState: {
+    type: String,
+    default: 'lobby',
+    required: true
+  },
+  gameCategory: String,
+  roomPassword: String,
+  roundTime: Number,
+  players: [
+    {
+      name: String,
+      points: Number,
+      guessed: Boolean,
+      role: {
+        type: String,
+        default: 'user'
+      }
+    }
+  ],
+  guessPhrase: String,
+  drawing: {
+    movesList: [
+      {
+        xStart: Number,
+        xEnd: Number,
+        yStart: Number,
+        yEnd: Number,
+        color: String,
+        size: Number
+      }
+    ],
+    moveCount: Number
+  },
 });
 
 module.exports = mongoose.model('Room', roomSchema);
