5eb3a8b119508c87f23218130c3a10f5d530e41d
Artur Witkowski <a.witkowski99@gmail.com>
2020-07-14 20:26:20 +0200

    Added About and Author pages, css tweaks

--- controllers/index.js
+++ controllers/index.js
@@ -1,5 +1,17 @@
 exports.getMainPage = (req, res, next) => {
-    return res.render('index/main-page');
+    return res.render('index/main-page', {
+        path: "/"
+    });
 }
 
+exports.getAboutPage = (req, res, next) => {
+    return res.render('index/about-page', {
+        path: "/about"
+    });
+}
 
+exports.getAuthorPage = (req, res, next) => {
+    return res.render('index/author-page', {
+        path: "/author"
+    });
+}
--- routes/index.js
+++ routes/index.js
@@ -7,4 +7,10 @@ const router = express.Router();
 // GET /
 router.get('/', indexController.getMainPage);
 
+// GET /
+router.get('/about', indexController.getAboutPage);
+
+// GET /
+router.get('/author', indexController.getAuthorPage);
+
 module.exports = router;
